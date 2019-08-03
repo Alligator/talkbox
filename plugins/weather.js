943fbcb65344f6b9f14a64d3302cf2516417e7e5
alligator <enemy.forest.brigade@gmail.com>
2019-08-03 23:24:44 +0100

    added weather command

---
+++ plugins/weather.js
@@ -0,0 +1,25 @@
+const jsweather = require('jsweather');
+const config = require('../config.json');
+
+async function weather(text, message) {
+  const name = message.author.username;
+  let location = text;
+
+  if (location && location.length > 0) {
+    db.write(name, location);
+  } else if (db.has(name)) {
+    location = db.read(name);
+  } else {
+    return 'no saved location';
+  }
+
+  const img = await jsweather.getWeather(location, config.bing_key, config.dark_sky_key);
+  message.channel.send({
+    files: [{
+      attachment: img,
+      name: 'weather.gif',
+    }],
+  });
+}
+
+commands = { weather };
