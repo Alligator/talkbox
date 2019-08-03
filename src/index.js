e0730d85ec6a52a6a70cfccb3fec55884df4e690
alligator <enemy.forest.brigade@gmail.com>
2019-08-03 15:44:56 +0100

    start adding repl commands

--- src/index.js
+++ src/index.js
@@ -8,13 +8,69 @@ const parseCommands = require('./command-parser');
 
 const config = require('../config.json');
 
-const pw = new PluginWatcher();
+let pw = new PluginWatcher();
 const client = new Discord.Client();
+
+async function runCommands(commands, message) {
+  let currentOutput = null;
+  for (let i = 0; i < commands.length; i++) {
+    const cmd = commands[i];
+
+    if (currentOutput === null) {
+      currentOutput = cmd.args.join(' ');
+    }
+
+    if (typeof currentOutput === 'undefined') {
+      // we got undefined somewhere, kill the pipe we're
+      // in a weird state
+      break;
+    }
+
+    const plugin = pw.getPlugin(cmd.commandName);
+    if (plugin) {
+      try {
+        currentOutput = await plugin(currentOutput, message);
+      } catch (e) {
+        logger.log(`error running command ${cmd.commandName} ${e}`);
+        logger.log(e.stack);
+        throw new Error(`command ${cmd.commandName} failed`);
+      }
+    } else {
+      throw new Error(`unknown command ${cmd.commandName}`);
+    }
+  }
+  return currentOutput;
+}
 
 client.on('ready', () => {
   logger.info('login successful');
   pw.startIntervals(client);
-  repl.start('tone> ').context.client = client;
+  const replServer = repl.start('talkbox> ');
+  replServer.context.client = client;
+  replServer.defineCommand('plugin', async function (cmd) {
+    const commands = parseCommands(cmd);
+    try {
+      const result = await runCommands(commands);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+    replServer.displayPrompt();
+  });
+
+  replServer.defineCommand('reloadPlugins', function () {
+    pw.stopIntervals();
+    pw = new PluginWatcher();
+    pw.watchFiles();
+    pw.startIntervals();
+    replServer.displayPrompt();
+  });
+
+  replServer.defineCommand('guilds', function () {
+    const guilds = client.guilds.map(guild => guild.name);
+    console.log(guilds);
+    replServer.displayPrompt();
+  });
 });
 
 client.on('disconnect', () => {
@@ -45,39 +101,14 @@ client.on('message', async (message) => {
   }
 
   const commands = parseCommands(messageText);
-
-  let currentOutput = null;
-  for (let i = 0; i < commands.length; i++) {
-    const cmd = commands[i];
-
-    if (currentOutput === null) {
-      currentOutput = cmd.args.join(' ');
+  try {
+    const result = await runCommands(commands, message);
+    if (result) {
+      message.channel.send(result);
     }
-
-    if (typeof currentOutput === 'undefined') {
-      // we got undefined somewhere, kill the pipe we're
-      // in a weird state
-      break;
-    }
-
-    const plugin = pw.getPlugin(cmd.commandName);
-    if (plugin) {
-      try {
-        currentOutput = await plugin(currentOutput, message);
-      } catch (e) {
-        message.channel.send(`command ${cmd.commandName} failed`);
-        logger.log(`error running command ${cmd.commandName} ${e}`);
-        logger.log(e.stack);
-        return;
-      }
-    } else {
-      message.channel.send(`unknown command ${cmd.commandName}`);
-      return;
-    }
-  }
-
-  if (currentOutput) {
-    message.channel.send(currentOutput);
+  } catch (e) {
+    message.channel.send(e.msg);
+    return;
   }
 
 });
