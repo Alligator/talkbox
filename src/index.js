354db1438922abdbdbc66c648286a035d2eb3b26
alligator <enemy.forest.brigade@gmail.com>
2019-08-10 11:23:18 +0100

    add an event handler for discord.js socket errors so they don't crash the bot
    added some other event handlers too for logging

--- src/index.js
+++ src/index.js
@@ -130,6 +130,11 @@ client.on('message', async (message) => {
   message.channel.stopTyping();
 });
 
+client.on('reconnecting', () => logger.info('attempting to reconnect to discord'));
+client.on('resume', () => logger.info('reconnected'));
+client.on('disconnect', () => logger.error('disconnected from discord'));
+client.on('error', e => logger.error(e.message));
+
 pw.watchFiles();
 
 logger.info('logging in to discord');
