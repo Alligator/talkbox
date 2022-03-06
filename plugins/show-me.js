ff3c2ab9f51b23b493832bf0114c330ad3efdeac
rrs <enemy.forest.brigade@gmail.com>
2022-03-06 13:40:11 +0000

    show me: more bs

--- plugins/show-me.js
+++ plugins/show-me.js
@@ -9,4 +9,22 @@ async function showMe(match, message) {
   message.reply({ files: [attachment] });
 }
 
-showMe._regex = /computer, show me (?:a )?(.*)/;
+function thanks(match, message) {
+  message.react('😉');
+}
+
+async function bad(match, message) {
+  await message.react('\uD83C\uDDEB');
+  await message.react('\uD83C\uDDFA');
+  await message.react('\uD83C\uDDE8');
+  await message.react('\uD83C\uDDF0');
+}
+
+async function tea(match, message) {
+  await message.react('\u2615');
+}
+
+showMe._regex = /computer, (?:show me|scroll up) (?:a )?(.*)/;
+thanks._regex = /((thank you)|(thanks)),? computer/;
+bad._regex = /bad? computer/;
+tea._regex = /computer, tea, earl grey, hot/;
